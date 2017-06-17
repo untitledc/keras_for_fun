02601973ef3cc9478c4bdd0a9bc53e1dbe0f6dec
untitledc <untitled.c@gmail.com>
2017-06-18 02:54:46 +0800

    overfit with my init via man-crafted function...

--- 2d_classify_graph/nn_illustration.py
+++ 2d_classify_graph/nn_illustration.py
@@ -24,6 +24,25 @@ def build_simple_regressor(model, hidden_dim=10):
     model.add(Dense(1, activation='linear'))
 
 
+def my_init_w1(shape, dtype=None):
+    return np.array([[1, 1, 1, 1, 1, 1, 1],
+                     [0, 0, 0, 0, 0, 0, 0]], dtype=float)
+
+
+def my_init_b1(shape, dtype=None):
+    return np.array([0, -0.2, -0.4, -0.4, -0.6, -0.8, -0.8])
+
+
+def my_init_w2(shape, dtype=None):
+    return np.array([5, -10, 5, 5, -10, 5, 5], dtype=float).reshape(7, 1)
+
+
+def build_my_init_regressor(model):
+    model.add(Dense(10, activation='relu', input_dim=2,
+                    kernel_initializer=my_init_w1, bias_initializer=my_init_b1))
+    model.add(Dense(1, activation='linear', kernel_initializer=my_init_w2))
+
+
 def build_mlp_relu_model(model, n_class, hidden_dim=2, layer_num=1):
     model.add(Dense(hidden_dim, activation='relu', input_dim=2))
     for i in range(layer_num-1):
@@ -64,7 +83,7 @@ def build_mess_around_model2(model, n_class):
 
 def main():
     is_classified = False
-    print_weight = False
+    print_weight = True
 
     #X, y = demo_dataset.get_breast_cancer_last2()
     #X, y = demo_dataset.get_iris()
@@ -82,7 +101,14 @@ def main():
     #build_simple_relu_model(model, n_class, 20)
     #build_mlp_relu_model(model, n_class, 200, 3)
     #build_mlp_relu_model(model, n_class, 200, 3)
-    build_simple_regressor(model, 200)
+    #build_simple_regressor(model, 10)
+    build_my_init_regressor(model)
+
+    print('initial weights')
+    for model_weight in model.get_weights():
+        print(model_weight.shape)
+        print(model_weight)
+
     if is_classified:
         model.compile(optimizer='sgd', loss='categorical_crossentropy',
                       metrics=['accuracy'])
@@ -95,7 +121,8 @@ def main():
         model.fit(X, y,
                   batch_size=1,
                   epochs=50,
-                  shuffle=True)
+                  shuffle=True,
+                  verbose=0)
 
     print('model summary')
     model.summary()
@@ -114,16 +141,23 @@ def main():
     draw_plot(model, title, plot_name, X, y, reso_step=0.005)
 
     #print(model.predict(np.array([[0.3, 0.4]])))
+    print('prediction')
     print(model.predict(np.array([[0.0, 0.0]])))
     print(model.predict(np.array([[0.2, 0.0]])))
     print(model.predict(np.array([[0.4, 0.0]])))
     print(model.predict(np.array([[0.6, 0.0]])))
     print(model.predict(np.array([[0.8, 0.0]])))
     print(model.predict(np.array([[1.0, 0.0]])))
-    # for test_in in np.arange(0, 1.2, 0.2):
-    #     o1_before = test_in * model_weight_list[0][0] + model_weight_list[1]
-    #     o1 = np.array([b if b > 0 else 0 for b in o1_before])
-    #     print(o1.dot(model_weight_list[2])+model_weight_list[3])
+
+    #print(model_weight_list[0].shape)
+    #print(model_weight_list[1].shape)
+    #print(model_weight_list[2].shape)
+    #print(model_weight_list[3].shape)
+
+    #for test_in in np.arange(0, 1.2, 0.2):
+    #    o1_before = test_in * model_weight_list[0][0] + model_weight_list[1]
+    #    o1 = np.array([b if b > 0 else 0 for b in o1_before])
+    #    print(o1.dot(model_weight_list[2])+model_weight_list[3])
 
 
 if __name__ == '__main__':
